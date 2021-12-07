921a5099bae4331ea50a634dda54491bba045a1f
wxx123 <stellarwxx@163.com>
2021-12-07 23:21:09 +0800

    make build container interactive to terminate successfully

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -16,5 +16,5 @@ IMAGE=linux-kernel-dev
 BUILD_DIR=/tmp/linux-kernel
 
 docker build -t $IMAGE --build-arg USER_NAME=$USER_NAME --build-arg USER_ID=$USER_ID .
-docker run -t --rm -v $PWD:$BUILD_DIR $IMAGE bash -c \
+docker run -ti --rm -v $PWD:$BUILD_DIR $IMAGE bash -c \
 	"source scl_source enable devtoolset-7 && make -C $BUILD_DIR $TARGET"
