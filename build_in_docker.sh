d8cb758d9115ecd166933bdf27dd671cba1a563a
wxx213 <stellarwxx@163.com>
2021-12-30 01:13:11 -0500

    make centos-rootfs work for build_in_docker.sh

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -2,9 +2,12 @@
 
 set -x -e
 
-TARGET=$1
-if [ -z $TARGET ]; then
-	TARGET=all
+TARGETS=$1
+
+if [ -z $TARGETS ]; then
+	TARGETS="install"
+elif [ $TARGETS == "all" ]; then
+	TARGETS="kernel qemu-x centos-rootfs"
 fi
 
 PWD=`pwd`
@@ -18,10 +21,14 @@ BUILD_DIR=/tmp/linux-kernel
 
 CENTOS_PREPARED="no"
 
-if [ $TARGET == centos-rootfs ]; then
-	./prepare_centos.sh $PWD/out/centos
-	CENTOS_PREPARED="yes"
-fi
 docker build -t $IMAGE --build-arg USER_NAME=$USER_NAME --build-arg USER_ID=$USER_ID -f Dockerfile.build .
-docker run -ti --rm -v $PWD:$BUILD_DIR $IMAGE bash -c \
-		"source scl_source enable devtoolset-7 && make CENTOS_PREPARED=$CENTOS_PREPARED -C $BUILD_DIR $TARGET"
+
+for TARGET in $TARGETS; do
+	if [ $TARGET == centos-rootfs ]; then
+		docker run -ti --rm -v $PWD:$BUILD_DIR $IMAGE bash -c "mkdir -p $BUILD_DIR/out && chmod +666 $BUILD_DIR/out"
+		./prepare_centos.sh $PWD/out/centos
+		CENTOS_PREPARED="yes"
+	fi
+	docker run -ti --rm -v $PWD:$BUILD_DIR $IMAGE bash -c \
+			"source scl_source enable devtoolset-7 && make CENTOS_PREPARED=$CENTOS_PREPARED -C $BUILD_DIR $TARGET"
+done
